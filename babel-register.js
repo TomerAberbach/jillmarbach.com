badc7212841a7b56df71c800d5d866c089bdbe22
Tomer Aberbach <tomeraberbach@gmail.com>
2021-01-04 18:26:13 -0500

    build: remove dependency on old babel preset

--- babel-register.js
+++ babel-register.js
@@ -1,15 +1,13 @@
 require(`@babel/register`)({
   babelrc: false,
-  presets: [`@tomera/node`],
+  presets: [`@babel/env`],
   plugins: [
     `@babel/transform-modules-commonjs`,
     [`module-resolver`, { root: [`./src`] }],
     `@babel/proposal-class-properties`,
     `@babel/proposal-export-default-from`,
     `@babel/proposal-export-namespace-from`,
-    `@babel/proposal-nullish-coalescing-operator`,
     `@babel/proposal-numeric-separator`,
-    `@babel/proposal-optional-chaining`,
     `@babel/proposal-throw-expressions`,
   ],
 })
